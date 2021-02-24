cecc59090cddfacbe7efdcfd8955c02e04bd252d
maxerMU <max_mitsevich@mail.ru>
2021-02-24 18:47:30 +0300

    lab_03

---
+++ sem_04/asm/lab_03/asm_afd.asm
@@ -0,0 +1,26 @@
+StkSeg SEGMENT PARA STACK 'STACK'
+    DB      200h DUP (?)
+StkSeg  ENDS
+DataS   SEGMENT WORD 'DATA'
+HelloMessage    DB   13
+                DB   10
+                DB   'Hello, world'
+                DB '$'
+DataS Ends
+Code    SEGMENT WORD 'CODE'
+        ASSUME  CS:Code, DS:DataS
+DispMsg:
+    mov AX,DataS
+    mov DS,AX
+    mov DX,OFFSET HelloMessage
+    mov AH,9
+    mov CX,3
+label1:
+    int 21h
+    loop label1
+    mov AH,7
+    int 21h
+    mov AH,4Ch
+    int 21h
+Code    ENDS
+        END DispMsg
